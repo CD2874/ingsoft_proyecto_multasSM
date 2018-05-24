fb43067501bfbfc6faf5a0108700bb63befac9be
Carlos Galvez <cgalvezb1@miumg.edu.gt>
2018-05-24 16:54:01 -0600

    hkj

--- app/src/test/java/com/example/cdgal/proyecto_ingsoft/consultas/ANTES/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('hostname', 'localhost');
-define('user', 'id4850517_prueba_api');
-define('password', 'abcd1234');
-define('db_name', 'id4850517_prueba_api');
-?>
--- app/src/test/java/com/example/cdgal/proyecto_ingsoft/consultas/resp - copia/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('hostname', 'localhost');
-define('user', 'id4850517_prueba_api');
-define('password', '123456');
-define('db_name', 'id4850517_prueba_api');
-?>
