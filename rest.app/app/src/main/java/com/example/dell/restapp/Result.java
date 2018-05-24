63d360a548d0db87ce3ea83faf538d3b23a9aaf4
Carlos Galvez <cgalvezb1@miumg.edu.gt>
2018-05-24 17:29:43 -0600

    FInal

---
+++ rest.app/app/src/main/java/com/example/dell/restapp/Result.java
@@ -0,0 +1,36 @@
+package com.example.dell.restapp;
+
+import java.io.Serializable;
+
+/**
+ * Created by DELL on 8/04/2017.
+ */
+
+public class Result{
+    String status;
+    String message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
